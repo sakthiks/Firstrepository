d6d9654780d62bf34dada3b3a976be2a94df7e41
sakthi-pt5327 <sakthi-pt5327@sakthi-pt5327.csez.zohocorpin.com>
2022-04-28 11:34:12 +0530

    merging

---
+++ merge.java
@@ -0,0 +1,16 @@
+package Firstrepository;
+
+public class merge {
+
+    public static void main(String [] args) {
+        int a=22;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+
+
+
+
+    }
+    
+}
