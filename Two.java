659865c807733edfbcbfe48a9d023a1946df55fc
sakthiks <81819039+sakthiks@users.noreply.github.com>
2022-04-28 10:46:04 +0530

    Hello

--- Two.java
+++ Two.java
@@ -2,7 +2,7 @@ package Firstrepository;
 class Two{
    
     public static void main(String [] args) {
-        int a=100;
+        int a=10000;
         int b=2;
         int c;
         System.out.println("Hello world");
