0127eb500850440194b29fccaacacf2d320b7b2e
sakthi-pt5327 <sakthi-pt5327@sakthi-pt5327.csez.zohocorpin.com>
2022-04-28 10:00:21 +0530

    First commit

---
+++ Two.java
@@ -0,0 +1,12 @@
+package Firstrepository;
+class Two{
+   
+    public static void main(String [] args) {
+        int a=1;
+        int b=2;
+        int c;
+        System.out.println("Hello world");
+        c=a+b;
+        System.out.println(c);
+    }
+}
